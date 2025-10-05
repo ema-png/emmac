0ca428295a5716f5e101ffebc4b3165ad8a2d4aa
Emma <ewalker3138@gmail.com>
2025-10-04 17:55:33 -0700

    website text/colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-      <h1 className="text-4xl font-bold text-red-600 mb-6">
-        ✧˖°. Emma's first Next.js & Tailwind Page ⋆ ˚｡⋆୨୧˚
+      <h1 className="text-4xl font-bold text-pink-400 mb-6">
+        ⋆ ˚｡⋆୨୧˚ Emma's first Next.js & Tailwind Page ⋆ ˚｡⋆୨୧˚
       </h1>
-      <p className="max-w-prose text-lg text-gray-700 text-center mb-6">
-        Hi! My name is Emma Walker, and I'm a freshman majoring in Computer Science. Please let me into this club 🥺
+      <p className="text-lg text-gray-500 mb-6">
+        Hi! My name is Emma Walker, and I'm a freshman majoring in Computer Science. 
       </p>
-      <button className="px-6 py-3 bg-red-500 text-white rounded-lg shadow hover:bg-orange-400 transition">
-        °❀⋆.ೃ࿔*:･
+      <div className="bg-pink-200 text-lg text-red-400 mb-6">
+        Fun Fact: I sprained my ankle under a seesaw when I was 7 
+      </div>
+      <button className="px-6 py-3 bg-red-300 text-white rounded-lg shadow hover:bg-pink-300 transition">
+        ‧₊˚❀༉‧₊˚.
       </button>
-      {/* <img src="/next.svg" alt="Next.js Logo" className="h-16 mt-6" />    */}
-      <img src="upp.png" alt="" className="h-20 mt-6=" />
-      </main>
+    </main>
   );
 }
